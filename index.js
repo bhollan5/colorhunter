76a19d37528352f8f1a4ab2093cffd50f515661c
Ben Holland <bhollan5@kent.edu>
2017-08-21 04:05:52 -0400

    Returns correct data

--- index.js
+++ index.js
@@ -1,54 +1,98 @@
+// Written by Ben Holland
+
 const express = require('express');
 // var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
+var cors = require('cors');
+
+
+app.use(cors());
+
+//app.addHeader("Access-Control-Allow-Origin", "*");
 
 app.get('/', function (req, res) {
-  var colors = {
-      c1: "#ffffff",
-      c2: "#ababab",
-      c3: "#777777",
-      c4: "#111111"
-  }
-  
-    var url = 'http://colorhunt.co/c/95352';
-
+    res.header('Access-Control-Allow-Origin', '*');
+    
+    function getColorJson(url) {
     request(url, function(error, response, html){
+        
+        console.log(response.statusCode);
 
         if(!error){
 
+            // Parses html via cheerioJS
+            
             var $ = cheerio.load(html);
 
-            // Finally, we'll define the variables we're going to capture
-
-            var title, release, rating;
-            var json = { id : "hm", 
+            var json = { id : "", 
                         code : "", 
                         date : "", 
-                        likes: ""
+                        likes: "",
+                        c1: "",
+                        c2: "",
+                        c3: "",
+                        c4: ""
                        };
                         
             var script = $('#jscode').next().html();
             
             var itemerIndex = script.search("itemer");
             
+            if (itemerIndex === -1) {
+                console.log(url + "didn't work.");
+                
+                return false;
+                //return getColorJson(Math.floor(Math.random() * 100000))
+            }
+            
             var itemerString = script.substr(itemerIndex, itemerIndex + 100);
             
             var itemer = itemerString.split("'");
-            console.log(itemer);
+            //console.log(itemer);
             
             json.id = itemer[1];
-            json.fullcode = itemer[3];
+            json.code = itemer[3];
             json.date = itemer[5];
             json.likes = itemer[7];
+            json.c1 = "#" + json.code.substring(0,6); 
+            json.c2 = "#" + json.code.substring(6,12); 
+            json.c3 = "#" + json.code.substring(12,18); 
+            json.c4 = "#" + json.code.substring(18,24);
   
-        res.json(json)
+            console.log("In return: " + json);
+             res.json(json);
+            return true;
         }
   
     })
+    }
+  
+    // Loads in colorhunt url via request
+        
+    var id = Math.floor(Math.random() * 100000);
     
+    var url = 'http://colorhunt.co/c/' + id.toString();
+    
+//    var json = getColorJson(url);
+    
+    while (getColorJson('http://colorhunt.co/c/' + Math.floor(Math.random() * 100000).toString())) {
+        
+    }
+    
+//    console.log("trying url : " + url);
+//    
+//    console.log("Outside:")
+//    console.log(json);
+    
+    
+    
+    
+
 });
+
+
 
 app.listen(3000, function () {
   console.log('Data returned to your host, on port 3000!')
